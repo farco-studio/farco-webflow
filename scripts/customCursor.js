a48fbe9539a4d3790f0146aa6a89abf136cfb75c
Marina Gallardo <marinagallardo@gmail.com>
2024-07-08 14:25:09 +0200

    custom cursor script update

--- scripts/customCursor.js
+++ scripts/customCursor.js
@@ -14,3 +14,26 @@ function showCursor() {
 }
 
 document.addEventListener("click", hideCursor);
+
+$(document).ready(function () {
+  gsap.set(".cursor-wrapper", { opacity: 0 });
+
+  function showCursor() {
+    gsap.to(".cursor-wrapper", { opacity: 1, duration: 0.4 });
+  }
+
+  function hideCursor() {
+    gsap.to(".cursor-wrapper", { opacity: 0, duration: 0.4 });
+  }
+
+  document.addEventListener("mousemove", function () {
+    showCursor();
+    document.removeEventListener("mousemove", showCursor, { once: true });
+  });
+
+  let timeout;
+  document.addEventListener("mousemove", function () {
+    clearTimeout(timeout);
+    timeout = setTimeout(hideCursor, 3000);
+  });
+});
